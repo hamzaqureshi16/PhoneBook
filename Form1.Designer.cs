abf7e3c0723cf396a2502c5be4ccdcbdf208da10
Hamza Qureshi <m.hamzaqureshi15@gmail.com>
2023-01-09 22:53:39 +0500

    imporovements

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -59,102 +59,109 @@
             this.panel1.Controls.Add(this.EmailLabel);
             this.panel1.Controls.Add(this.NameLabel);
             this.panel1.Controls.Add(this.PhoneLabel);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(14, 16);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(522, 194);
+            this.panel1.Size = new System.Drawing.Size(597, 259);
             this.panel1.TabIndex = 1;
             // 
             // AddressInput
             // 
             this.AddressInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.AddressInput.Location = new System.Drawing.Point(115, 116);
+            this.AddressInput.Location = new System.Drawing.Point(131, 155);
+            this.AddressInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.AddressInput.Multiline = true;
             this.AddressInput.Name = "AddressInput";
-            this.AddressInput.Size = new System.Drawing.Size(383, 70);
+            this.AddressInput.Size = new System.Drawing.Size(437, 93);
             this.AddressInput.TabIndex = 7;
             // 
             // EmailInput
             // 
             this.EmailInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.EmailInput.Location = new System.Drawing.Point(115, 82);
+            this.EmailInput.Location = new System.Drawing.Point(131, 109);
+            this.EmailInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.EmailInput.Name = "EmailInput";
             this.EmailInput.PlaceholderText = "e.g johnsmith@domain.com";
-            this.EmailInput.Size = new System.Drawing.Size(383, 23);
+            this.EmailInput.Size = new System.Drawing.Size(437, 27);
             this.EmailInput.TabIndex = 6;
             // 
             // NameInput
             // 
             this.NameInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.NameInput.Location = new System.Drawing.Point(115, 47);
+            this.NameInput.Location = new System.Drawing.Point(131, 63);
+            this.NameInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.NameInput.Name = "NameInput";
             this.NameInput.PlaceholderText = "e.g John Smith";
-            this.NameInput.Size = new System.Drawing.Size(261, 23);
+            this.NameInput.Size = new System.Drawing.Size(298, 27);
             this.NameInput.TabIndex = 5;
             // 
             // NumberInput
             // 
             this.NumberInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.NumberInput.Location = new System.Drawing.Point(115, 12);
+            this.NumberInput.Location = new System.Drawing.Point(131, 16);
+            this.NumberInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.NumberInput.Name = "NumberInput";
             this.NumberInput.PlaceholderText = "E.g 03335037773";
-            this.NumberInput.Size = new System.Drawing.Size(189, 23);
+            this.NumberInput.Size = new System.Drawing.Size(216, 27);
             this.NumberInput.TabIndex = 4;
             // 
             // AdressLabel
             // 
-            this.AdressLabel.Location = new System.Drawing.Point(3, 116);
+            this.AdressLabel.Location = new System.Drawing.Point(3, 155);
             this.AdressLabel.Name = "AdressLabel";
-            this.AdressLabel.Size = new System.Drawing.Size(71, 20);
+            this.AdressLabel.Size = new System.Drawing.Size(81, 27);
             this.AdressLabel.TabIndex = 3;
             this.AdressLabel.Text = "Address:";
             // 
             // EmailLabel
             // 
-            this.EmailLabel.Location = new System.Drawing.Point(3, 82);
+            this.EmailLabel.Location = new System.Drawing.Point(3, 109);
             this.EmailLabel.Name = "EmailLabel";
-            this.EmailLabel.Size = new System.Drawing.Size(71, 20);
+            this.EmailLabel.Size = new System.Drawing.Size(81, 27);
             this.EmailLabel.TabIndex = 2;
             this.EmailLabel.Text = "Email:";
             // 
             // NameLabel
             // 
-            this.NameLabel.Location = new System.Drawing.Point(3, 47);
+            this.NameLabel.Location = new System.Drawing.Point(3, 63);
             this.NameLabel.Name = "NameLabel";
-            this.NameLabel.Size = new System.Drawing.Size(71, 20);
+            this.NameLabel.Size = new System.Drawing.Size(81, 27);
             this.NameLabel.TabIndex = 1;
             this.NameLabel.Text = "Full Name:";
             // 
             // PhoneLabel
             // 
-            this.PhoneLabel.Location = new System.Drawing.Point(3, 12);
+            this.PhoneLabel.Location = new System.Drawing.Point(3, 16);
             this.PhoneLabel.Name = "PhoneLabel";
-            this.PhoneLabel.Size = new System.Drawing.Size(106, 20);
+            this.PhoneLabel.Size = new System.Drawing.Size(121, 27);
             this.PhoneLabel.TabIndex = 0;
             this.PhoneLabel.Text = "Phone Number:";
             this.PhoneLabel.Click += new System.EventHandler(this.PhoneLabel_Click);
             // 
             // SearchLabel
             // 
-            this.SearchLabel.Location = new System.Drawing.Point(15, 223);
+            this.SearchLabel.Location = new System.Drawing.Point(17, 297);
             this.SearchLabel.Name = "SearchLabel";
-            this.SearchLabel.Size = new System.Drawing.Size(77, 23);
+            this.SearchLabel.Size = new System.Drawing.Size(88, 31);
             this.SearchLabel.TabIndex = 2;
             this.SearchLabel.Text = "Search:";
             this.SearchLabel.Click += new System.EventHandler(this.label1_Click);
             // 
             // ToBeSearched
             // 
-            this.ToBeSearched.Location = new System.Drawing.Point(127, 223);
+            this.ToBeSearched.Location = new System.Drawing.Point(145, 297);
+            this.ToBeSearched.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ToBeSearched.Name = "ToBeSearched";
-            this.ToBeSearched.PlaceholderText = "Phone number";
-            this.ToBeSearched.Size = new System.Drawing.Size(306, 23);
+            this.ToBeSearched.PlaceholderText = "Full Name";
+            this.ToBeSearched.Size = new System.Drawing.Size(349, 27);
             this.ToBeSearched.TabIndex = 3;
             // 
             // NewButton
             // 
-            this.NewButton.Location = new System.Drawing.Point(220, 417);
+            this.NewButton.Location = new System.Drawing.Point(251, 556);
+            this.NewButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.NewButton.Name = "NewButton";
-            this.NewButton.Size = new System.Drawing.Size(75, 23);
+            this.NewButton.Size = new System.Drawing.Size(86, 31);
             this.NewButton.TabIndex = 6;
             this.NewButton.Text = "New";
             this.NewButton.UseVisualStyleBackColor = true;
@@ -163,22 +170,22 @@
             // dataGrid
             // 
             this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGrid.Location = new System.Drawing.Point(15, 256);
-            this.dataGrid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGrid.Location = new System.Drawing.Point(17, 341);
             this.dataGrid.MultiSelect = false;
             this.dataGrid.Name = "dataGrid";
             this.dataGrid.RowHeadersWidth = 51;
             this.dataGrid.RowTemplate.Height = 29;
             this.dataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGrid.Size = new System.Drawing.Size(524, 155);
+            this.dataGrid.Size = new System.Drawing.Size(599, 207);
             this.dataGrid.TabIndex = 7;
             this.dataGrid.SelectionChanged += new System.EventHandler(this.dataGrid_SelectionChanged);
             // 
             // EditButton
             // 
-            this.EditButton.Location = new System.Drawing.Point(301, 417);
+            this.EditButton.Location = new System.Drawing.Point(344, 556);
+            this.EditButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.EditButton.Name = "EditButton";
-            this.EditButton.Size = new System.Drawing.Size(75, 23);
+            this.EditButton.Size = new System.Drawing.Size(86, 31);
             this.EditButton.TabIndex = 8;
             this.EditButton.Text = "Edit";
             this.EditButton.UseVisualStyleBackColor = true;
@@ -186,9 +193,10 @@
             // 
             // CancelButton
             // 
-            this.CancelButton.Location = new System.Drawing.Point(382, 417);
+            this.CancelButton.Location = new System.Drawing.Point(437, 556);
+            this.CancelButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.CancelButton.Name = "CancelButton";
-            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.Size = new System.Drawing.Size(86, 31);
             this.CancelButton.TabIndex = 9;
             this.CancelButton.Text = "Cancel";
             this.CancelButton.UseVisualStyleBackColor = true;
@@ -196,9 +204,10 @@
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(459, 417);
+            this.SaveButton.Location = new System.Drawing.Point(525, 556);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.Size = new System.Drawing.Size(86, 31);
             this.SaveButton.TabIndex = 10;
             this.SaveButton.Text = "Save";
             this.SaveButton.UseVisualStyleBackColor = true;
@@ -206,9 +215,10 @@
             // 
             // SearchButton
             // 
-            this.SearchButton.Location = new System.Drawing.Point(458, 223);
+            this.SearchButton.Location = new System.Drawing.Point(523, 297);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.SearchButton.Name = "SearchButton";
-            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.Size = new System.Drawing.Size(86, 31);
             this.SearchButton.TabIndex = 11;
             this.SearchButton.Text = "Search";
             this.SearchButton.UseVisualStyleBackColor = true;
@@ -216,9 +226,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(545, 450);
+            this.ClientSize = new System.Drawing.Size(623, 600);
             this.Controls.Add(this.SearchButton);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.CancelButton);
@@ -228,6 +238,7 @@
             this.Controls.Add(this.ToBeSearched);
             this.Controls.Add(this.SearchLabel);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
